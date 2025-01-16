7547f9b5bb7cb8684269ce6123f028bfa8f54f13
rohit <rp4714443@gmail.com>
2025-01-16 20:17:13 +0530

    this is repo

--- dockerfile
+++
@@ -1,6 +0,0 @@
-FROM ubuntu
-RUN apt-get update
-RUN apt-get install apache2 -y
-COPY index.html /var/www/html
-EXPOSE 80
-CMD ["apache2ctl",-D",FOREGROUND"]
--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-sudo apt-update
-sudoo apt-get install docker.io -y
-sudo system start enable.docker
-
-sudo docker build -t img1 /home/ubuntu/app/
-
-sudo docker run -d --name container1 -p 80:80 img1
+sudo yum install httpd -y
+sudo service start httpd
